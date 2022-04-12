a946ecff94cc96d033c79ece56493c55cb3050c4
Natalia Betancourt <nataliabetancourtm@gmail.com>
2022-04-12 12:54:05 -0500

    Start screens ready

--- view/main.js
+++ view/main.js
@@ -1,7 +1,56 @@
+//Classes
+let introScreens;
+
+//Variables
+let screen;
+
 function setup() {
   createCanvas(1280, 720);
+
+  //Calling classes
+  introScreens = new StartScreens();
+
+  //Variables
+  this.screen = 0;
 }
 
 function draw() {
   background(220);
+
+  //Switch between class screens
+  switch (this.screen) {
+    //Start screens
+    case 0:
+      introScreens.draw();
+      introScreens.hover();
+      break;
+    //Symptoms screen
+    case 1:
+      
+      break;
+  }
+
+  switchBetweenClasses();
+  
 }
+
+function mousePressed() {
+  switch (this.screen) {
+    //Start screens clicks
+    case 0:
+      introScreens.clicked();
+      break;
+    case 1:
+      
+      break;
+  }
+
+}
+
+function switchBetweenClasses() {
+  //Start screen to symptoms
+  if (introScreens.getScreenClicked()) {
+    this.screen = 1;
+  }
+}
+
---
+++ view/start-screens.js
@@ -0,0 +1,125 @@
+//All the start up screens
+class StartScreens {
+    constructor(){
+        //Loading background images
+        this.firstScreen = loadImage('../images/start_screen.jpg');
+        this.secondScreen = loadImage('../images/intro.jpg');
+        this.thirdScreen = loadImage('../images/instructions.jpg');
+
+        //Loading button images
+        this.startBtn = loadImage('../images/start_btn.png');
+        this.continueBtn = loadImage('../images/continue_btn.png');
+
+        //Screen variable
+        this.screen = 0;
+        this.screenClicked = false;
+        this.screenSwitch = 0;
+
+        //Buttons
+        this.showBtn = false;
+        this.btnCounter = 0;
+
+        //Buttons bigger
+        this.wStart = 250;
+        this.hStart = 80;
+
+        this.wCon = 250;
+        this.hCon = 80;
+
+        this.wCon2 = 250;
+        this.hCon2 = 80;
+    }
+
+    draw(){
+        //Transition between screens
+        switch (this.screen) {
+            case 0:
+                image(this.firstScreen, 0, 0, 1280, 720);
+                image(this.startBtn, 820, 385, this.wStart, this.hStart);
+                break;
+            case 1:
+                image(this.secondScreen, 0, 0, 1280, 720);
+
+                //Button counter
+                this.btnCounter++;
+                if (this.btnCounter > 300) {
+                    image(this.continueBtn, 885, 565, this.wCon, this.hCon);
+                }
+                break;
+            case 2:
+                image(this.thirdScreen, 0, 0, 1280, 720);
+
+                //Button counter
+                this.btnCounter++;
+                if (this.btnCounter > 300) {
+                    image(this.continueBtn, 885, 565, this.wCon2, this.hCon2); 
+                }
+                break;
+        }
+    }
+
+    //Animation, kinda
+    hover() {
+        //Start button
+        if (this.mouse(820, 1070, 385, 465) && this.screen === 0) {
+            this.wStart = 280;
+            this.hStart = 90;
+        } else {
+            this.wStart = 250;
+            this.hStart = 80;
+        }
+
+        //Continue button 1
+        if (this.mouse(885, 1135, 565, 640) && this.screen === 1) {
+            this.wCon = 280;
+            this.hCon = 90;
+        } else {
+            this.wCon = 250;
+            this.hCon = 80;
+        }
+
+        //Continue button 1
+        if (this.mouse(885, 1135, 565, 640) && this.screen === 2) {
+            this.wCon2 = 280;
+            this.hCon2 = 90;
+        } else {
+            this.wCon2 = 250;
+            this.hCon2 = 80;
+        }
+    }
+
+    //When clicking on buttons, switch screens
+    clicked() {
+        //Start button to switch to introduction screen
+        if (this.mouse(820, 1070, 385, 465) && this.screen === 0) {
+            this.screen = 1;
+        }
+
+        if (this.mouse(885, 1135, 565, 640) && this.screen === 1) {
+            this.screen = 2;
+            this.btnCounter = 0;
+        }
+
+        if (this.mouse(885, 1135, 565, 640) && this.screen === 2) {
+            this.btnCounter = 0;
+            this.screenSwitch++;
+            if (this.screenSwitch === 2) {
+                this.screenClicked = true;
+                this.screenSwitch = 0;
+            }
+        }
+    }
+
+    //Condition for dimensions
+    mouse(x1, x2, y1, y2) {
+        if (mouseX > x1 && mouseX < x2 && mouseY > y1 && mouseY < y2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    getScreenClicked() {
+        return this.screenClicked;
+    }
+}
